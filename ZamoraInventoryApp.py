1f0852f4cfa16724feac90e3cd14b914ac91f227
adm15123 <aliant.delgado07@gmail.com>
2025-08-16 20:27:11 -0400

    Update ZamoraInventoryApp.py

--- ZamoraInventoryApp.py
+++ ZamoraInventoryApp.py
@@ -46,12 +46,12 @@ app.config["UPLOAD_FOLDER"] = config.UPLOAD_FOLDER
 # Flask-Mail and Login Configuration
 # -------------------------------
 
-app.config["MAIL_SERVER"] = config.MAIL_SERVER
-app.config["MAIL_PORT"] = config.MAIL_PORT
-app.config["MAIL_USE_TLS"] = config.MAIL_USE_TLS
-app.config["MAIL_USERNAME"] = config.MAIL_USERNAME
-app.config["MAIL_PASSWORD"] = config.MAIL_PASSWORD
-app.config["MAIL_DEFAULT_SENDER"] = config.MAIL_DEFAULT_SENDER
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = "aliant.delgado07@gmail.com"
+app.config["MAIL_PASSWORD"] = "lgco kmqe emqr qdrj"  # Use an app-specific password if using 2FA
+app.config["MAIL_DEFAULT_SENDER"] = "aliant.delgado07@gmail.com"
 
 mail = Mail(app)
 serializer = URLSafeTimedSerializer(app.secret_key)
